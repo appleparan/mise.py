639570d4c7d51a16f356e989c00b8a9b75a7a337
Jongsu Liam Kim <jongsukim8@gmail.com>
2020-05-12 21:28:57 +0900

    Split load function for raw data and imputed data

--- mise/data.py
+++ mise/data.py
@@ -1,9 +1,26 @@
 import pandas as pd
 
-def load(filepath="/input/input.csv"):
+def load(filepath="/input/input.csv", datecol=[1]):
+    # date column in raw data : 1
+    # date column in imputed data : 0
     df = pd.read_csv(filepath,
-        index_col = [0, 1],
-        parse_dates = [1])
+        index_col=[0, 1],
+        parse_dates=datecol)
+
+    # prints
+    pd.set_option('display.max_rows', 10)
+    pd.reset_option('display.max_rows')
+    print(df.head(10))
+
+    return df
+
+
+def load_imputed(filepath="/input/input.csv", datecol=[0]):
+    # date column in raw data : 1
+    # date column in imputed data : 0
+    df = pd.read_csv(filepath,
+                     index_col=[0, 1],
+                     parse_dates=datecol)
 
     # prints
     pd.set_option('display.max_rows', 10)
