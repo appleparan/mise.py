2012cb96800280f06ae55c6b0132419786d72704
Jongsu Liam Kim <jongsukim8@gmail.com>
2020-12-07 08:43:26 +0900

    Remove debug flag

--- mise/ml/rnn_mul_tpa_attn.py
+++ mise/ml/rnn_mul_tpa_attn.py
@@ -64,8 +64,8 @@ def ml_rnn_mul_tpa_attn(station_name="종로구"):
     sample_size = 48
     output_size = 24
     # If you want to debug, fast_dev_run = True and n_trials should be small number
-    fast_dev_run = True
-    n_trials = 1
+    fast_dev_run = False
+    n_trials = 100
 
     # Hyper parameter
     epoch_size = 500
