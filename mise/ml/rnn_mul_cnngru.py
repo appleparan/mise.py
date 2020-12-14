4ae49b5ea68803ee64d17ddef2fb50710d039df7
Jongsu Liam Kim <jongsukim8@gmail.com>
2020-12-15 04:41:19 +0900

    Reduce max_epochs as 35, 50 is too much

--- mise/ml/rnn_mul_cnngru.py
+++ mise/ml/rnn_mul_cnngru.py
@@ -144,7 +144,7 @@ def ml_rnn_mul_cnngru(station_name="종로구"):
             # most basic trainer, uses good defaults
             trainer = Trainer(gpus=1 if torch.cuda.is_available() else None,
                               precision=32,
-                              min_epochs=1, max_epochs=50,
+                              min_epochs=1, max_epochs=35,
                               early_stop_callback=PyTorchLightningPruningCallback(
                                   trial, monitor="val_loss"),
                               default_root_dir=output_dir,
